d22f0656d5bdc0803f65217ae2866665275ab5cc
ba2hankaya <baa2han.kay@gmail.com>
2024-10-26 13:34:10 +0300

    moved the pick a random card from sim to here

--- simulator/deck.cpp
+++ simulator/deck.cpp
@@ -1,6 +1,34 @@
 #include <vector>
 #include "deck.h"
+#include <stdlib.h>
+#include <chrono>
 using namespace std;
+
+
+using namespace std::chrono;
+uint64_t timeSinceEpochMillisec() {
+	using namespace std::chrono;
+	return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+card PickARandomCard(vector<card>& deck){
+	srand(timeSinceEpochMillisec());
+	if(deck.size() == 0){
+		return EmptyCard;
+	}
+
+	//if(deck.size() == 0){
+	//	card x;
+	//	x.value = 10;
+	//	CardSuit s = Spades; 
+	//	x.suit = s;
+	//	return x;
+	//}
+	int index = rand() % deck.size();
+	card mycard = deck[index];
+	deck.erase(deck.begin()+index);
+	return mycard;
+}
 
 vector<card> CreateDeck(){
 	vector<card> deck;
