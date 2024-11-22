5519333ef1eed75987d6171f66a4b4fcd78ae99f
ba2hankaya <baa2han.kay@gmail.com>
2024-11-23 02:04:49 +0300

    add parser, change file naming system, fix range check bug, major QOL

--- graphs/grapher.py
+++ graphs/grapher.py
@@ -1,13 +1,48 @@
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(
+        prog='GraphDrawer',
+        description='Draws graphs using datasets'
+        )
+
+parser.add_argument(
+        'files',
+        nargs='+',
+        help='files or file to process'
+        )
+parser.add_argument(
+        '-m',
+        type=int,
+        choices=[0,1],
+        default=1,
+        help="1 for scatter, 0 for line (def = 1))"
+        )
+parser.add_argument(
+        '-o',
+        help = "output file"
+        )
+parser.add_argument(
+        '-l',
+        action="store_true",
+        help= "label the data(include -l to label, omit to not)"
+        )
+parser.add_argument(
+        '-r',
+        type=int,
+        help='range of numattempts to scale the graph'
+        )
+
+args = parser.parse_args()
 
 def scaleData(data, _range):
     if _range == 0:
         return data;
     dmin = min(data)
     dmax = max(data)
-    if dmax - dmin == range:
+    if dmax - dmin == _range:
         return data;
     scaled = []
     for value in data:
@@ -16,7 +51,7 @@ def scaleData(data, _range):
 
 
 def plotFile(filename):
-    _range = int(sys.argv[1])
+    _range = args.r
     f = open(filename, "r")
     lines = []
     #readfile
@@ -29,28 +64,24 @@ def plotFile(filename):
         scores.append(int(lines[index].split(' ')[0]))
         numOfAttempts.append(int(lines[index].split(' ')[2]))
     numOfAttempts = scaleData(numOfAttempts, _range)
-    if mode == 1:
-        if not labelbool:
-            plt.plot(np.array(scores),np.array(numOfAttempts))
-        else:
-            plt.plot(np.array(scores),np.array(numOfAttempts),label=filename[:-4])
-    elif mode == 2:
-        if not labelbool:  
-            plt.plot(np.array(scores),np.array(numOfAttempts))
-        else:
-            plt.plot(np.array(scores),np.array(numOfAttempts),label=filename[:-4])
+    x = np.array(scores)
+    y = np.array(numOfAttempts)
+    filename = filename.split('/')[-1]
+    if args.m == True:
+        plt.scatter(x,y,label=filename)
+    else:
+        plt.plot(x,y,label=filename)
+    plt.title(args.o)
 
-mode = sys.argv[-1]
-labelbool = sys.argv[-2]
-for arg in sys.argv[2:-3]:
-    plotFile(arg)
+for filename in args.files:
+    plotFile(filename)
 plt.locator_params(axis='y', nbins=20)
 plt.xticks([*range(0,25)])
 plt.grid()
 plt.xlabel("Scores")
 plt.ylabel("Num of Attempts")
-if labelbool:
+if args.l:
     plt.legend()
 fig = plt.gcf()
-fig.savefig(sys.argv[-1] + "_graph.png")
+fig.savefig(args.o)
 
